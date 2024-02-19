90780d1d084d882a56e22eced6db9363de157587
seongbeom <sb@seongbeomui-MacBookAir.local>
2024-02-19 15:09:06 +0900

    amazon datasets statistics yaml

--- run.py
+++
@@ -1,7 +0,0 @@
-from recbole.config import Config
-from recbole.quick_start import run_recbole
-# parameter_dict={
-#     'data_path':'/Users/sb/Desktop/project/preference_rec/dataset',
-#     'train_neg_sample_args':None,
-# }
-run_recbole(model='SASRec', dataset='Amazon_Sports_and_Outdoors', config_file_list=['sasrec.yaml'])
