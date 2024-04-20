afe702e7aedda890dbcabebe604c0c31895ade2c
seongbeom <psm981021@naver.com>
2024-04-20 20:32:03 +0900

    Experiment del

--- scripts/Beauty/Cluster_Attention_IntentCL.sh
+++ scripts/Beauty/Cluster_Attention_IntentCL.sh
@@ -1,29 +1,33 @@
-for augment_type in mask
-do
-    for gamma in 0.7 
-    do
-        python main.py \
-            --model_name="UPTRec" \
-            --data_name="Beauty"  \
-            --output_dir="output_custom/Beauty" \
-            --contrast_type="IntentCL" \
-            --seq_representation_type="concatenate" \
-            --attention_type="Cluster" \
-            --model_idx="UPTRec_Clustered_Attention_IntentCL_${augment_type}_${gamma}" \
-            --augment_type=$augment_type \
-            --gamma=$gamma \
-            --num_intent_clusters=16 --gpu_id=0 --epochs=1000 --patience=500 --warm_up_epochs=40 --de_noise
+# for augment_type in mask
+# do
+#     for gamma in 0.7 
+#     do
+#         python main.py \
+#             --model_name="UPTRec" \
+#             --data_name="Beauty"  \
+#             --output_dir="output_custom/Beauty" \
+#             --contrast_type="IntentCL" \
+#             --seq_representation_type="concatenate" \
+#             --attention_type="Cluster" \
+#             --model_idx="UPTRec_Clustered_Attention_IntentCL_${augment_type}_${gamma}" \
+#             --augment_type=$augment_type \
+#             --gamma=$gamma \
+#             --num_intent_clusters=16 --gpu_id=0 --epochs=1000 --patience=500 --warm_up_epochs=40 --de_noise
 
-    done
-done
+#     done
+# done
 
 
-# python main.py \
-#     --model_name="UPTRec" \
-#     --data_name="Beauty"  \
-#     --output_dir="output_custom/Beauty/Cluster_Attention_IntentCL" \
-#     --contrast_type="IntentCL" \
-#     --seq_representation_type="concatenate" \
-#     --attention_type="Cluster" \
-#     --model_idx="UPTRec_Clustered_Attention_IntentCL" \
-#     --num_intent_clusters=16 --gpu_id=0 --epochs=1000 --patience=500 --warm_up_epoches=40 --do_eval --embedding
+python main.py \
+    --model_name="UPTRec" \
+    --data_name="Beauty"  \
+    --augment_type="mask" \
+    --gamma=0.7 \
+    --n_views=3 \
+    --temperature=0.5 \
+    --output_dir="output_custom/Beauty/Cluster_Attention_IntentCL" \
+    --contrast_type="IntentCL" \
+    --seq_representation_type="concatenate" \
+    --attention_type="Cluster" \
+    --model_idx="UPTRec_Clustered_Attention_IntentCL_${augment_type}_${gamma}" \
+    --num_intent_clusters=16 --gpu_id=1 --epochs=1000 --patience=500 --warm_up_epoches=40 #--do_eval --embedding
