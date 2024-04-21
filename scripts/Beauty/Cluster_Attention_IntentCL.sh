546c8a7a6a134d019f0b3b1b1d2aa797a0f63f1e
seongbeom <psm981021@naver.com>
2024-04-21 17:59:33 +0900

    Experiment temperature

--- scripts/Beauty/Cluster_Attention_IntentCL.sh
+++ scripts/Beauty/Cluster_Attention_IntentCL.sh
@@ -9,10 +9,11 @@ do
             --contrast_type="IntentCL" \
             --seq_representation_type="concatenate" \
             --attention_type="Cluster" \
-            --model_idx="UPTRec_Clustered_Attention_IntentCL_${augment_type}_${gamma}" \
+            --model_idx="UPTRec_Clustered_Attention_IntentCL_${augment_type}_${gamma}_nviews_3_temperature_0.3" \
             --augment_type=$augment_type \
             --gamma=$gamma \
-            --num_intent_clusters=8 --temperature=0.5 --gpu_id=1 --epochs=1000 --patience=500 --warm_up_epoches=40 \
+            --n_views=3\
+            --num_intent_clusters=16 --temperature=0.3 --gpu_id=1 --epochs=1000 --patience=500 --warm_up_epoches=40 \
 
     done
 done
