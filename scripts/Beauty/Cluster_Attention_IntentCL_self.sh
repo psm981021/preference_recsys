1937769535caba03f20bdcda8bec0571357553f5
seongbeom <psm981021@naver.com>
2024-04-29 13:04:14 +0900

    Experiment add Day 14

---
+++ scripts/Beauty/Cluster_Attention_IntentCL_self.sh
@@ -0,0 +1,26 @@
+for augment_type in mask
+do
+    for gamma in 0.7
+    do
+        python main.py \
+            --model_name="UPTRec" \
+            --data_name="Beauty"  \
+            --output_dir="output_custom/Beauty/Cluster_Attention_IntentCL" \
+            --contrast_type="IntentCL" \
+            --context="encoder" \
+            --seq_representation_type="concatenate" \
+            --attention_type="Cluster" \
+            --augment_type=$augment_type \
+            --gamma=$gamma \
+            --batch_size=512 \
+            --gpu_id=1 \
+            --n_views=2 \
+            --epochs=2000 \
+            --intent_cf_weight=0.5 \
+            --temperature=1.0 \
+            --patience=500 \
+            --warm_up_epoches=200 \
+            --num_intent_clusters=128 \
+            --model_idx="UPTRec_Clustered_Attention_IntentCL_${augment_type}_${gamma}_encoder_self-attention"
+    done
+done
